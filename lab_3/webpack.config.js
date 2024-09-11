fb17724ca4a38edaa839314d87fcbc071a45aa13
X3ros7 <a.a.prosv2005@gmail.com>
2024-09-11 16:54:59 +0300

    Fix table

--- lab_3/webpack.config.js
+++ lab_3/webpack.config.js
@@ -1,50 +1,44 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: `[name].bundle.js`,
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Особистий блог',
-            template: path.resolve(__dirname, './src/pages/index.html'), 
-            filename: 'pages/index.html', 
-            
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Розклад',
-            template: path.resolve(__dirname, './src/pages/rozklad.html'), 
-            filename: 'pages/rozklad.html', 
-            
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Фотоальбом',
-            template: path.resolve(__dirname, './src/pages/photo.html'), 
-            filename: 'pages/photo.html', 
-            
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Останні новини',
-            template: path.resolve(__dirname, './src/pages/news.html'), 
-            filename: 'pages/news.html', 
-            
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: 'src/assets', to: 'assets' }
-            ]
-        })
-    ],
-    devServer: {
-        port: 8080,
-    }
-}
+  mode: "development",
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: `[name].bundle.js`,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Особистий блог",
+      template: path.resolve(__dirname, "./src/pages/index.html"),
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Розклад",
+      template: path.resolve(__dirname, "./src/pages/rozklad.html"),
+      filename: "rozklad.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Фотоальбом",
+      template: path.resolve(__dirname, "./src/pages/photo.html"),
+      filename: "photo.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Останні новини",
+      template: path.resolve(__dirname, "./src/pages/news.html"),
+      filename: "news.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets" }],
+    }),
+  ],
+  devServer: {
+    port: 8080,
+  },
+};
